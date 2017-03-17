a867842adddd4cec49d91e1dde2d6bb00b195460
mityakoval <dima.alex.koval@gmail.com>
2017-03-17 20:46:26 +0100

    updated implementation to meet assignment requirements

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,9 +1,8 @@
+
 /**
  * 
  */
 
-
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
@@ -11,101 +10,114 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  *
  */
 public class Percolation {
-	
-	private Site[][] grid = null;
-	private int virtualTopId;
-	private int virtualBottomId;
-	private int openSites = 0;
-	private WeightedQuickUnionUF wqu = null;
-	
-	public Percolation(int n) {
-		if(n <= 0) throw new IllegalArgumentException("n should be greater than 0");
-		grid = new Site[n][n];
-		for(int i=0; i < n ; i++) {
-			for(int j=0; j < n ; j++) {
-				int index = ((n - 1) * i) + j + i;
-				Site site = new Site(index);
-				grid[i][j] = site;
-			}
-			
-		}
-		
-		virtualTopId = n*n;
-		virtualBottomId = n*n + 1;
-		wqu = new WeightedQuickUnionUF(n*n + 2);
-		for(int i=0; i < n ; i++){
-			wqu.union(i, virtualTopId);
-			wqu.union((n*n - n + i), virtualBottomId);
-		}
-	}
-	
-	public void open(int row, int col) {
-		row--;
-		col--;
-		if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
-			throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
-		}
-		Site site = grid[row][col];
-		if (site.isOpen()) return;
-		site.open();
-		openSites++;
-		connectNeighbour(site, row-1, col);
-		connectNeighbour(site, row+1, col);
-		connectNeighbour(site, row, col-1);
-		connectNeighbour(site, row, col+1);
-	}
-	
-	public boolean isOpen(int row, int col) {
-		row--;
-		col--;
-		if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
-			throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
-		}
-		return grid[row][col].isOpen();
-	}
-	
-	public boolean isFull(int row, int col) {
-		row--;
-		col--;
-		if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
-			throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
-		}
-		Site site = grid[row][col];
-		return wqu.connected(site.getIndex(), virtualTopId);
-	}
-	
-	public int numberOfOpenSites() {
-		return openSites;
-	}
-	
-	public boolean percolates() {
-		return wqu.connected(virtualTopId, virtualBottomId);
-	}
-	
-	private void connectNeighbour(Site site, int row, int col){
-		if ((row >= 0 && row < grid.length) && (col >= 0 && col < grid.length)) {
-			Site nbSite = grid[row][col];
-			if(nbSite.isOpen()){
-				wqu.union(site.getIndex(), nbSite.getIndex());
-			}
-		}
-	}
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Percolation p = new Percolation(100);
-		int i = 1;
-		while(!p.percolates()) {
-			System.out.println(i);
-			int row = StdRandom.uniform(1, 101);
-			int col = StdRandom.uniform(1, 101);
-			p.open(row, col);
-			i++;
-		}
-		System.out.println(p.numberOfOpenSites());
-	}
+    private Site[][] grid = null;
+    private int virtualTopId;
+    private int virtualBottomId;
+    private int openSites = 0;
+    private WeightedQuickUnionUF wqu = null;
 
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("n should be greater than 0");
+        grid = new Site[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int index = ((n - 1) * i) + j + i;
+                Site site = new Site(index);
+                grid[i][j] = site;
+            }
+
+        }
+
+        virtualTopId = n * n;
+        virtualBottomId = n * n + 1;
+        wqu = new WeightedQuickUnionUF(n * n + 2);
+        for (int i = 0; i < n; i++) {
+            wqu.union(i, virtualTopId);
+            wqu.union((n * n - n + i), virtualBottomId);
+        }
+    }
+
+    public void open(int row, int col) {
+        row--;
+        col--;
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
+            throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
+        }
+        Site site = grid[row][col];
+        if (site.isOpen())
+            return;
+        site.open();
+        openSites++;
+        connectNeighbour(site, row - 1, col);
+        connectNeighbour(site, row + 1, col);
+        connectNeighbour(site, row, col - 1);
+        connectNeighbour(site, row, col + 1);
+    }
+
+    public boolean isOpen(int row, int col) {
+        row--;
+        col--;
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
+            throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
+        }
+        return grid[row][col].isOpen();
+    }
+
+    public boolean isFull(int row, int col) {
+        row--;
+        col--;
+        if ((row < 0 || row >= grid.length) || (col < 0 || col >= grid.length)) {
+            throw new IndexOutOfBoundsException("Coordinates are out of the bounds of the grid");
+        }
+        Site site = grid[row][col];
+        return (site.isOpen() && wqu.connected(site.getIndex(), virtualTopId));
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return wqu.connected(virtualTopId, virtualBottomId);
+    }
+
+    private void connectNeighbour(Site site, int row, int col) {
+        if ((row >= 0 && row < grid.length) && (col >= 0 && col < grid.length)) {
+            Site nbSite = grid[row][col];
+            if (nbSite.isOpen()) {
+                wqu.union(site.getIndex(), nbSite.getIndex());
+            }
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+    
+    private class Site {
+        private int index;
+        private boolean open;
+
+        public Site(int index) {
+            this.index = index;
+            this.open = false;
+        }
+
+        public boolean isOpen() {
+            return this.open;
+        }
+
+        public void open() {
+            this.open = true;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,6 +1,5 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.Stopwatch;
 
 /**
  * 
@@ -11,66 +10,59 @@ import edu.princeton.cs.algs4.Stopwatch;
  *
  */
 public class PercolationStats {
-	private int trials;
-	private double[] thresholds;
-	private Double mean = null;
-	private Double stddev = null;
-	
-	public PercolationStats(int n, int trials) {
-		if (n <= 0 || trials <= 0) throw new IllegalArgumentException();
-		this.trials = trials;
-		this.thresholds = new double[trials];
-		Stopwatch stopwatch = new Stopwatch();
-		int i = 0;
-		for(; i < trials ; i++) {
-			Percolation p = new Percolation(n);
-			while(!p.percolates()) {
-				int row = StdRandom.uniform(1, n+1);
-				int col = StdRandom.uniform(1, n+1);
-				p.open(row, col);
-			}
-			double threshold = (double) p.numberOfOpenSites() / (n*n);
-			thresholds[i] = threshold;
-		}
-		System.out.println("trials " + i);
-		System.out.println("time\t\t\t\t= " + stopwatch.elapsedTime() + "s");
-	}
-	
-	public double mean() {
-		mean = StdStats.mean(thresholds);
-		return mean;
-	}
-	
-	public double stddev() {
-		stddev = StdStats.stddev(thresholds);
-		return stddev;
-	}
-	
-	public double confidenceLo() {
-		if (mean == null) mean();
-		if (stddev == null) stddev();
-		return (mean - (1.96 * Math.sqrt(stddev) / Math.sqrt(trials)));
-	}
-	
-	public double confidenceHi() {
-		if (mean == null) mean();
-		if (stddev == null) stddev();
-		return (mean + (1.96 * Math.sqrt(stddev) / Math.sqrt(trials)));
-	}
+    private int trials;
+    private double[] thresholds;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n = Integer.parseInt(args[0]);
-		int trials = Integer.parseInt(args[1]);
-		PercolationStats ps = new PercolationStats(n, trials);
-		
-		System.out.println("mean\t\t\t\t= " + ps.mean());
-		System.out.println("stddev\t\t\t\t= " + ps.stddev());
-		System.out.println("95% confidence interval\t\t= [" + ps.confidenceLo() + 
-						   ", " + ps.confidenceHi() + "]");
-	}
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException();
+        this.trials = trials;
+        this.thresholds = new double[trials];
+        int i = 0;
+        for (; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                p.open(row, col);
+            }
+            double threshold = (double) p.numberOfOpenSites() / (n * n);
+            thresholds[i] = threshold;
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    public double confidenceLo() {
+        double mean = mean(); 
+        double stddev = stddev();
+        return (mean - (1.96 * stddev / Math.sqrt(trials)));
+    }
+
+    public double confidenceHi() {
+        double mean = mean(); 
+        double stddev = stddev();
+        return (mean + (1.96 * stddev / Math.sqrt(trials)));
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+
+        System.out.println("mean\t\t\t= " + ps.mean());
+        System.out.println("stddev\t\t\t= " + ps.stddev());
+        System.out.println("95% confidence interval\t= [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 
 }
